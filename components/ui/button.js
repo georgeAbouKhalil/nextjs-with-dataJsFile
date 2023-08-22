48ef0f42f5dcc0638323cf1f9aad06d6171ee99d
georgeAbouKhalil <georgeak752@gmail.com>
2023-08-22 23:52:07 +0300

    add styles

---
+++ components/ui/button.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import classes from "./buttoom.module.css";
+
+function Button(props) {
+  if (props.link) {
+    return (
+      // <Link href={props.link}>
+      //   <a className={classes.btn}>{props.children}</a>
+      // </Link>
+      <Link className={classes.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+  }
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
